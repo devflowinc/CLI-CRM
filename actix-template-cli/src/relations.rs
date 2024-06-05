1c63c400fca24ebb1f5418bccea5a000f26e78e7
Drew Harris <harrisd@smu.edu>
2024-06-05 15:14:55 -0500

    expand relation definitions

--- actix-template-cli/src/relations.rs
+++ actix-template-cli/src/relations.rs
@@ -1,7 +1,8 @@
-use actix_web_starter_client::models::{Note, Task};
+use actix_web_starter_client::models::Note;
 
-use crate::errors::DefaultError;
+use crate::{commands::configure::ActixTemplateConfiguration, errors::DefaultError};
 
+// Either phone, contact etc... will have a bunch of matches
 pub trait Relation {
     fn fn_get_extension_path(&self) -> String;
 }
@@ -10,5 +11,64 @@ pub trait HasRelations<D>
 where
     D: Relation,
 {
-    fn get_relation<T>(&self, other: &str) -> Result<T, DefaultError>;
+    fn get_relations<T>(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: D,
+    ) -> Result<Vec<T>, DefaultError>;
+    fn add_relation(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: D,
+        other_id: String,
+    ) -> Result<(), DefaultError>;
+    fn delete_relation(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: D,
+        other_id: String,
+    ) -> Result<(), DefaultError>;
+}
+
+pub enum NoteRelation {
+    Task,
+    Contact,
+}
+
+impl Relation for NoteRelation {
+    fn fn_get_extension_path(&self) -> String {
+        match self {
+            NoteRelation::Task => "tasks".to_string(),
+            NoteRelation::Contact => "contacts".to_string(),
+        }
+    }
+}
+
+impl HasRelations<NoteRelation> for Note {
+    // not implemented
+    fn get_relations<T>(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: NoteRelation,
+    ) -> Result<Vec<T>, DefaultError> {
+        unimplemented!()
+    }
+
+    fn add_relation(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: NoteRelation,
+        other_id: String,
+    ) -> Result<(), DefaultError> {
+        unimplemented!()
+    }
+
+    fn delete_relation(
+        &self,
+        config: ActixTemplateConfiguration,
+        relation: NoteRelation,
+        other_id: String,
+    ) -> Result<(), DefaultError> {
+        unimplemented!()
+    }
 }
